69ea949eff2d5b148d46feb2886fcb5de21c80d0
ChaimaeK <chaimae.kharbouch8@etu.univ-lorraine.fr>
2022-04-12 13:30:26 +0200

    Create Admin.Model.ts

---
+++ Version finale/testbind/src/app/Model/Admin.Model.ts
@@ -0,0 +1,10 @@
+export class Admin {
+  constructor(
+    public id_user:number,
+    public email: string,
+    public full_name: string,
+    public password: string,
+    public telephone: number,
+
+  ) {}
+}
